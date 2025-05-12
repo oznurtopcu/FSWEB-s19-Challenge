32cdd968265b70cb1e17e2721f6bb4d2a8b45eda
oznurtopcu <topcuoznur121@gmail.com>
2025-05-12 14:41:40 +0300

    create project files

---
+++ src/main/java/com/workintech/s19challenge/S19challengeApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.s19challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S19challengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S19challengeApplication.class, args);
+	}
+
+}
