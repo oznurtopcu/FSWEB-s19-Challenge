fdf80a870f6f5e154201f796840f2542310eca52
oznurtopcu <topcuoznur121@gmail.com>
2025-05-13 00:59:54 +0300

    add user repository queries

--- src/main/java/com/workintech/s19challenge/repository/UserRepository.java
+++ src/main/java/com/workintech/s19challenge/repository/UserRepository.java
@@ -2,6 +2,17 @@ package com.workintech.s19challenge.repository;
 
 import com.workintech.s19challenge.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    //emailden kullanıcıyı bulalım
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findUserByEmail(String email);
+
+    //userNameden kullanıcıyı bulalım
+    @Query("SELECT u FROM User u WHERE u.userName = :userName")
+    Optional<User> findUserByUsername(String userName);
 }
