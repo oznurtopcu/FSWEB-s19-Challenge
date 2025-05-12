36f0c9b1c3aed7fcffa9363041c98f6f564c5f67
oznurtopcu <topcuoznur121@gmail.com>
2025-05-13 00:54:59 +0300

    add entity security implements

--- src/main/java/com/workintech/s19challenge/entity/user/Role.java
+++ src/main/java/com/workintech/s19challenge/entity/user/Role.java
@@ -3,13 +3,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
 @Table(name = "role", schema = "public")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/workintech/s19challenge/entity/user/User.java
+++ src/main/java/com/workintech/s19challenge/entity/user/User.java
@@ -8,15 +8,20 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
 @Table(name = "user", schema = "public")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,7 +30,7 @@ public class User {
 
     @Column(name = "user_name")
     @NotBlank(message = "Username cannot be empty!")
-    private String userName;
+    private String usernName;
 
     @Column(name = "email")
     @NotBlank(message = "Email cannot be empty!")
@@ -38,7 +43,49 @@ public class User {
     @Size(min=6, max = 100, message = "Password must be between 6 and 100 characters!")
     private String password;
 
+
     //User - Tweet
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private List<Tweet> tweets;
+
+    //User - Role
+    //Uni-directional bağ
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", schema = "public",
+            joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> authorities = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        //return UserDetails.super.isAccountNonExpired();
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        //return UserDetails.super.isAccountNonLocked();
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        //return UserDetails.super.isCredentialsNonExpired();
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        //return UserDetails.super.isEnabled();
+        return true;
+    }
 }
