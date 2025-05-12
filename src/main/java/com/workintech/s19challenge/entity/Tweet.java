cfc5dfbe42afd4952a11047612f259790a78235a
oznurtopcu <topcuoznur121@gmail.com>
2025-05-12 15:56:01 +0300

    create user and tweet entity

---
+++ src/main/java/com/workintech/s19challenge/entity/Tweet.java
@@ -0,0 +1,27 @@
+package com.workintech.s19challenge.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tweet", schema = "public")
+public class Tweet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    //User - Tweet
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ src/main/java/com/workintech/s19challenge/entity/User.java
@@ -0,0 +1,34 @@
+package com.workintech.s19challenge.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "user", schema = "public")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    //User - Tweet
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<Tweet> tweets;
+}
