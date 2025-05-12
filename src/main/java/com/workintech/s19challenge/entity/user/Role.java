11fab9b84e9f4ae2a980076c4d7067cc11777e37
oznurtopcu <topcuoznur121@gmail.com>
2025-05-13 00:45:36 +0300

    create role entity

---
+++ src/main/java/com/workintech/s19challenge/entity/user/Role.java
@@ -0,0 +1,21 @@
+package com.workintech.s19challenge.entity.user;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "role", schema = "public")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "authority")
+    private String authority;
+}
