1daaa943f177393635332dea1dedc5832a13d5a9
oznurtopcu <topcuoznur121@gmail.com>
2025-05-13 23:13:09 +0300

    create comment entity

---
+++ src/main/java/com/workintech/s19challenge/entity/Comment.java
@@ -0,0 +1,38 @@
+package com.workintech.s19challenge.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.workintech.s19challenge.entity.user.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "comment", schema = "public")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "comment")
+    @NotBlank(message = "Comment cannot be empty!")
+    private String comment;
+
+    //Comment - User
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
+
+    //Comment - Tweet
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "tweet_id")
+    @JsonBackReference
+    private Tweet tweet;
+}
