88cf82127fa7c0dca18a2c0f38cd1229a9365b44
oznurtopcu <topcuoznur121@gmail.com>
2025-05-14 00:01:41 +0300

    create comment endpoints

--- src/main/java/com/workintech/s19challenge/controller/CommentController.java
+++ src/main/java/com/workintech/s19challenge/controller/CommentController.java
@@ -1,8 +1,11 @@
 package com.workintech.s19challenge.controller;
 
+import com.workintech.s19challenge.dto.CommentRequest;
 import com.workintech.s19challenge.entity.Comment;
+import com.workintech.s19challenge.entity.user.User;
 import com.workintech.s19challenge.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,17 +20,20 @@ public class CommentController {
     }
 
     @PostMapping
-    public Comment create() {
-
+    public Comment create(@RequestBody CommentRequest commentRequest, Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        return commentService.create(commentRequest.tweet_id(), commentRequest.content(), user.getId());
     }
 
     @PutMapping("/{id}")
-    public Comment update(@PathVariable long id) {
-
+    public Comment update(@PathVariable long id, @RequestBody String content, Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        return commentService.update(id,content, user.getId());
     }
 
     @DeleteMapping("/{id}")
-    public Comment delete(@PathVariable long id) {
-
+    public Comment delete(@PathVariable long id, Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        return commentService.delete(id, user.getId());
     }
 }
