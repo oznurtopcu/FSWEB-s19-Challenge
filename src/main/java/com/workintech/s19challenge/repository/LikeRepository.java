76ab3c5278fce79e73d43225785cdd4b4fecd440
oznurtopcu <topcuoznur121@gmail.com>
2025-05-14 21:24:11 +0300

    add custom queries in like repository

--- src/main/java/com/workintech/s19challenge/repository/LikeRepository.java
+++ src/main/java/com/workintech/s19challenge/repository/LikeRepository.java
@@ -2,6 +2,19 @@ package com.workintech.s19challenge.repository;
 
 import com.workintech.s19challenge.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
+
+    @Query("SELECT l FROM Like l WHERE l.tweet.id = :tweet_id")
+    List<Like> findByTweetId(long tweet_id);
+
+    @Query("SELECT l FROM Like l WHERE l.user.id = :user_id")
+    List<Like> findByUserId(long user_id);
+
+    @Query("SELECT l FROM Like l WHERE l.tweet.id = :tweet_id AND l.user.id = :user_id")
+    Like findIsLiked(@Param("tweet_id") long tweet_id, @Param("user_id") long user_id);
 }
