7d3d64eee50db610ec55ee5e55b2ce3a0464b99a
oznurtopcu <topcuoznur121@gmail.com>
2025-05-14 14:32:08 +0300

    create like entity

---
+++ src/main/java/com/workintech/s19challenge/entity/Like.java
@@ -0,0 +1,31 @@
+package com.workintech.s19challenge.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.workintech.s19challenge.entity.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "like", schema = "public")
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "tweet_id")
+    @JsonBackReference
+    private Tweet tweet;
+}
