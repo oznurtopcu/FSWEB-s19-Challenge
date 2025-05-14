1b02f884ae7f0d284b2b63818a5f4268cd0dd5bc
oznurtopcu <topcuoznur121@gmail.com>
2025-05-14 14:38:09 +0300

    create like repository

---
+++ src/main/java/com/workintech/s19challenge/repository/LikeRepository.java
@@ -0,0 +1,7 @@
+package com.workintech.s19challenge.repository;
+
+import com.workintech.s19challenge.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+}
